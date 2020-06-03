7a9c15932973b9a9631b199489c76ed3f7604f02
Munkh-Od <eikyuu.hoshi@gmail.com>
2020-06-04 00:42:33 +0800

    Refactored Blog Single Page

--- resources/views/blogSingle.blade.php
+++ resources/views/blogSingle.blade.php
@@ -76,12 +76,6 @@
 					<!-- End Review -->
 				</div>
 				<ul class="nav justify-content-between mb-11">
-					<li class="nav-item m-0">
-						<a class="nav-link text-gray-27 px-0" href="../blog/single-blog-post.html"><span class="mr-1">←</span> SpaceX Falcon explodes after Landing</a>
-					</li>
-					<li class="nav-item m-0">
-						<a class="nav-link text-gray-27 px-0" href="../blog/single-blog-post.html">Announcement – Post without Image <span class="ml-1">→</span></a>
-					</li>
 				</ul>
 				<div class="mb-10">
 					<div class="border-bottom border-color-1 mb-10">
@@ -215,27 +209,6 @@
 			</aside>
 			<aside class="mb-7">
 				<div class="border-bottom border-color-1 mb-5">
-					<h3 class="section-title section-title__sm mb-0 pb-2 font-size-18">About Blog</h3>
-				</div>
-				<p class="text-gray-90 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt, erat in malesuada aliquam, est erat faucibus purus, eget viverra nulla sem vitae neque. Quisque id sodales libero.</p>
-			</aside>
-			<aside class="mb-7">
-				<div class="border-bottom border-color-1 mb-5">
-					<h3 class="section-title section-title__sm mb-0 pb-2 font-size-18">Categories</h3>
-				</div>
-				<div class="list-group">
-					<a href="../blog/single-blog-post.html" class="font-bold-on-hover px-3 py-2 list-group-item list-group-item-action border-0"><i class="mr-2 fas fa-angle-right"></i> Design</a>
-					<a href="../blog/single-blog-post.html" class="font-bold-on-hover px-3 py-2 list-group-item list-group-item-action border-right-0 border-left-0 border-bottom-0"><i class="mr-2 fas fa-angle-right"></i> Events</a>
-					<a href="../blog/single-blog-post.html" class="font-bold-on-hover px-3 py-2 list-group-item list-group-item-action border-right-0 border-left-0 border-bottom-0"><i class="mr-2 fas fa-angle-right"></i> Links & Quotes</a>
-					<a href="../blog/single-blog-post.html" class="font-bold-on-hover px-3 py-2 list-group-item list-group-item-action border-right-0 border-left-0 border-bottom-0"><i class="mr-2 fas fa-angle-right"></i> News</a>
-					<a href="../blog/single-blog-post.html" class="font-bold-on-hover px-3 py-2 list-group-item list-group-item-action border-right-0 border-left-0 border-bottom-0"><i class="mr-2 fas fa-angle-right"></i> Social</a>
-					<a href="../blog/single-blog-post.html" class="font-bold-on-hover px-3 py-2 list-group-item list-group-item-action border-right-0 border-left-0 border-bottom-0"><i class="mr-2 fas fa-angle-right"></i> Technology</a>
-					<a href="../blog/single-blog-post.html" class="font-bold-on-hover px-3 py-2 list-group-item list-group-item-action border-right-0 border-left-0 border-bottom-0"><i class="mr-2 fas fa-angle-right"></i> Audios</a>
-					<a href="../blog/single-blog-post.html" class="font-bold-on-hover px-3 py-2 list-group-item list-group-item-action border-right-0 border-left-0 border-bottom-0"><i class="mr-2 fas fa-angle-right"></i> Videos</a>
-				</div>
-			</aside>
-			<aside class="mb-7">
-				<div class="border-bottom border-color-1 mb-5">
 					<h3 class="section-title section-title__sm mb-0 pb-2 font-size-18">Recent Posts</h3>
 				</div>
 				<article class="mb-4">
@@ -295,79 +268,8 @@
 					</div>
 				</article>
 			</aside>
-			<aside class="mb-7">
-				<div class="border-bottom border-color-1 mb-5">
-					<h3 class="section-title section-title__sm mb-0 pb-2 font-size-18">Tags Clouds</h3>
-				</div>
-				<div class="d-flex flex-wrap mx-n1">
-					<a href="../blog/single-blog-post.html" class="text-gray-90 mb-2 bg-primary-on-hover py-2 px-3 border mx-1">amazon like</a>
-					<a href="../blog/single-blog-post.html" class="text-gray-90 mb-2 bg-primary-on-hover py-2 px-3 border mx-1">Awesome</a>
-					<a href="../blog/single-blog-post.html" class="text-gray-90 mb-2 bg-primary-on-hover py-2 px-3 border mx-1">bootstrap</a>
-					<a href="../blog/single-blog-post.html" class="text-gray-90 mb-2 bg-primary-on-hover py-2 px-3 border mx-1">buy it</a>
-					<a href="../blog/single-blog-post.html" class="text-gray-90 mb-2 bg-primary-on-hover py-2 px-3 border mx-1">clean design</a>
-					<a href="../blog/single-blog-post.html" class="text-gray-90 mb-2 bg-primary-on-hover py-2 px-3 border mx-1">electronics</a>
-					<a href="../blog/single-blog-post.html" class="text-gray-90 mb-2 bg-primary-on-hover py-2 px-3 border mx-1">theme</a>
-					<a href="../blog/single-blog-post.html" class="text-gray-90 mb-2 bg-primary-on-hover py-2 px-3 border mx-1">video post format</a>
-					<a href="../blog/single-blog-post.html" class="text-gray-90 mb-2 bg-primary-on-hover py-2 px-3 border mx-1">Ecommerce</a>
-					<a href="../blog/single-blog-post.html" class="text-gray-90 mb-2 bg-primary-on-hover py-2 px-3 border mx-1">wordpress</a>
-				</div>
-			</aside>
 		</div>
 	</div>
-	<!-- Brand Carousel -->
-	<div class="mb-6">
-		<div class="py-2 border-top border-bottom">
-			<div class="js-slick-carousel u-slick my-1" data-slides-show="5" data-slides-scroll="1" data-arrows-classes="d-none d-lg-inline-block u-slick__arrow-normal u-slick__arrow-centered--y"
-			 data-arrow-left-classes="fa fa-angle-left u-slick__arrow-classic-inner--left z-index-9" data-arrow-right-classes="fa fa-angle-right u-slick__arrow-classic-inner--right" data-responsive='[{
-                                "breakpoint": 992,
-                                "settings": {
-                                    "slidesToShow": 2
-                                }
-                            }, {
-                                "breakpoint": 768,
-                                "settings": {
-                                    "slidesToShow": 1
-                                }
-                            }, {
-                                "breakpoint": 554,
-                                "settings": {
-                                    "slidesToShow": 1
-                                }
-                            }]'>
-				<div class="js-slide">
-					<a href="#" class="link-hover__brand">
-						<img class="img-fluid m-auto max-height-50" src="/img/200X60/img1.png" alt="Image Description">
-					</a>
-				</div>
-				<div class="js-slide">
-					<a href="#" class="link-hover__brand">
-						<img class="img-fluid m-auto max-height-50" src="/img/200X60/img2.png" alt="Image Description">
-					</a>
-				</div>
-				<div class="js-slide">
-					<a href="#" class="link-hover__brand">
-						<img class="img-fluid m-auto max-height-50" src="/img/200X60/img3.png" alt="Image Description">
-					</a>
-				</div>
-				<div class="js-slide">
-					<a href="#" class="link-hover__brand">
-						<img class="img-fluid m-auto max-height-50" src="/img/200X60/img4.png" alt="Image Description">
-					</a>
-				</div>
-				<div class="js-slide">
-					<a href="#" class="link-hover__brand">
-						<img class="img-fluid m-auto max-height-50" src="/img/200X60/img5.png" alt="Image Description">
-					</a>
-				</div>
-				<div class="js-slide">
-					<a href="#" class="link-hover__brand">
-						<img class="img-fluid m-auto max-height-50" src="/img/200X60/img6.png" alt="Image Description">
-					</a>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- End Brand Carousel -->
 </div>
 
 @endsection
