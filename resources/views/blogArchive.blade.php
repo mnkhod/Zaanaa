d2ffcaf7abcdd9e603a630f4c4bb6e00f97d2ac8
Munkh-Od <eikyuu.hoshi@gmail.com>
2020-06-04 00:38:59 +0800

    Refactored Blog Archive Page

--- resources/views/blogArchive.blade.php
+++ resources/views/blogArchive.blade.php
@@ -249,20 +249,6 @@
 						</div>
 					</div>
 				</article>
-				<article class="card mb-13 border-0">
-					<div class="card-body p-0">
-						<blockquote class="blockquote">
-							<p>Quisque a commodo lectus. Nunc vel dolor sed libero venenatis egestas. Cras non volutpat enim. Cras molestie purus id lorem sodales, in facilisis erat tristique. Aliquam luctus orci aliquet augue eleifend.</p>
-							<footer class="blockquote-footer"><cite title="Source Title">Steve Kowalsky</cite></footer>
-						</blockquote>
-					</div>
-				</article>
-				<article class="card mb-8 border-0">
-					<div class="card-body p-5 p-md-10 bg-gray-1">
-						<p class="d-block font-size-25 text-gray-39">If you are looking for a developer on ThemeForest – check out the link</p>
-						<a href="../blog/single-blog-post.html" class="text-blue">http://wwww.transvelo.com</a>
-					</div>
-				</article>
 			</div>
 		</div>
 		<div class="col-xl-3 col-wd">
@@ -278,12 +264,6 @@
 						</button>
 					</div>
 				</form>
-			</aside>
-			<aside class="mb-7">
-				<div class="border-bottom border-color-1 mb-5">
-					<h3 class="section-title section-title__sm mb-0 pb-2 font-size-18">About Blog</h3>
-				</div>
-				<p class="text-gray-90 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt, erat in malesuada aliquam, est erat faucibus purus, eget viverra nulla sem vitae neque. Quisque id sodales libero.</p>
 			</aside>
 			<aside class="mb-7">
 				<div class="border-bottom border-color-1 mb-5">
@@ -361,79 +341,8 @@
 					</div>
 				</article>
 			</aside>
-			<aside class="mb-7">
-				<div class="border-bottom border-color-1 mb-5">
-					<h3 class="section-title section-title__sm mb-0 pb-2 font-size-18">Tags Clouds</h3>
-				</div>
-				<div class="d-flex flex-wrap mx-n1">
-					<a href="../blog/single-blog-post.html" class="text-gray-90 mb-2 bg-primary-on-hover py-2 px-3 border mx-1">amazon like</a>
-					<a href="../blog/single-blog-post.html" class="text-gray-90 mb-2 bg-primary-on-hover py-2 px-3 border mx-1">Awesome</a>
-					<a href="../blog/single-blog-post.html" class="text-gray-90 mb-2 bg-primary-on-hover py-2 px-3 border mx-1">bootstrap</a>
-					<a href="../blog/single-blog-post.html" class="text-gray-90 mb-2 bg-primary-on-hover py-2 px-3 border mx-1">buy it</a>
-					<a href="../blog/single-blog-post.html" class="text-gray-90 mb-2 bg-primary-on-hover py-2 px-3 border mx-1">clean design</a>
-					<a href="../blog/single-blog-post.html" class="text-gray-90 mb-2 bg-primary-on-hover py-2 px-3 border mx-1">electronics</a>
-					<a href="../blog/single-blog-post.html" class="text-gray-90 mb-2 bg-primary-on-hover py-2 px-3 border mx-1">theme</a>
-					<a href="../blog/single-blog-post.html" class="text-gray-90 mb-2 bg-primary-on-hover py-2 px-3 border mx-1">video post format</a>
-					<a href="../blog/single-blog-post.html" class="text-gray-90 mb-2 bg-primary-on-hover py-2 px-3 border mx-1">Ecommerce</a>
-					<a href="../blog/single-blog-post.html" class="text-gray-90 mb-2 bg-primary-on-hover py-2 px-3 border mx-1">wordpress</a>
-				</div>
-			</aside>
 		</div>
 	</div>
-	<!-- Brand Carousel -->
-	<div class="mb-6">
-		<div class="py-2 border-top border-bottom">
-			<div class="js-slick-carousel u-slick my-1" data-slides-show="5" data-slides-scroll="1" data-arrows-classes="d-none d-lg-inline-block u-slick__arrow-normal u-slick__arrow-centered--y"
-			 data-arrow-left-classes="fa fa-angle-left u-slick__arrow-classic-inner--left z-index-9" data-arrow-right-classes="fa fa-angle-right u-slick__arrow-classic-inner--right" data-responsive='[{
-                                "breakpoint": 992,
-                                "settings": {
-                                    "slidesToShow": 2
-                                }
-                            }, {
-                                "breakpoint": 768,
-                                "settings": {
-                                    "slidesToShow": 1
-                                }
-                            }, {
-                                "breakpoint": 554,
-                                "settings": {
-                                    "slidesToShow": 1
-                                }
-                            }]'>
-				<div class="js-slide">
-					<a href="#" class="link-hover__brand">
-						<img class="img-fluid m-auto max-height-50" src="/img/200X60/img1.png" alt="Image Description">
-					</a>
-				</div>
-				<div class="js-slide">
-					<a href="#" class="link-hover__brand">
-						<img class="img-fluid m-auto max-height-50" src="/img/200X60/img2.png" alt="Image Description">
-					</a>
-				</div>
-				<div class="js-slide">
-					<a href="#" class="link-hover__brand">
-						<img class="img-fluid m-auto max-height-50" src="/img/200X60/img3.png" alt="Image Description">
-					</a>
-				</div>
-				<div class="js-slide">
-					<a href="#" class="link-hover__brand">
-						<img class="img-fluid m-auto max-height-50" src="/img/200X60/img4.png" alt="Image Description">
-					</a>
-				</div>
-				<div class="js-slide">
-					<a href="#" class="link-hover__brand">
-						<img class="img-fluid m-auto max-height-50" src="/img/200X60/img5.png" alt="Image Description">
-					</a>
-				</div>
-				<div class="js-slide">
-					<a href="#" class="link-hover__brand">
-						<img class="img-fluid m-auto max-height-50" src="/img/200X60/img6.png" alt="Image Description">
-					</a>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- End Brand Carousel -->
 </div>
 
 @endsection
